3065f280a9c0398ef61df4957a880652bfad058e
Daniel Williams <daniel_williams@live.com>
2022-10-24 18:14:01 +0200

    Refactor nvim config into mostly lua + new plugin setup (without init.lua)

---
+++ .config/nvim/lua/remaps.lua
@@ -0,0 +1,32 @@
+vim.keymap.set('n', '<leader>ex', ':Ex<CR>')
+
+-- Move lines up/down in visual mode
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+vim.keymap.set('n', 'J', "mzJ'z") -- Keep cursor at beginning of line when joining
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+-- Keep search results in centre
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+vim.keymap.set('x', '<leader>p', '"_dP') -- TODO: Find out why ex mode??
+
+-- Yank into system clipboard with <leader>y
+vim.keymap.set('n', '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>Y', '"+Y')
+vim.keymap.set('v', '<leader>y', '"+y')
+
+-- Delete into void register with leader
+vim.keymap.set('n', '<leader>d', '"_d')
+vim.keymap.set('v', '<leader>d', '"_d')
+
+vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format() end)
+
+-- TODO: Checkout Quickfix (and implement Primeagen keymaps for it
+
+-- Replace all instaces of the word underneath the cursor
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Make current file executable (for scripts etc)
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
