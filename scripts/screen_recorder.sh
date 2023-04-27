0026e4b89068ffcbb55ff2f68117813f26b78714
Daniel Williams <daniel_williams@live.com>
2023-04-28 00:05:59 +0200

    Adapt screen recorder to work better with 4k + laptop

--- scripts/screen_recorder.sh
+++ scripts/screen_recorder.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-opts='1:  ¹ Record left screen\n2:  ² Record right screen\n3:  Pause\n4:  Resume\n5:  Stop'
-chosen_option=$(printf "$opts" | rofi -dmenu -p "")
+rec=""
+pause=""
+resume=""
+stop=""
+opts="$rec\n$pause\n$resume\n$stop"
+chosen_option=$(printf "$opts" | rofi -dmenu -p "Screen recording" -theme powermenu.rasi)
 filename="$HOME/recordings/$(date +'%Y-%m-%d-%H:%M:%S').mkv"
 
 get_opts(){
@@ -13,15 +17,14 @@ get_opts(){
 # -f x11grab makes the screen into an input device, :0.0 is the screen and the + coordinates are for
 # screens (in this case) 1920 pixels to the left of the absolute left (i.e. the second screen).
 case "$chosen_option" in
-    $(get_opts 1)) notify-send "Recording screen 1" "60fps / 1920x1080" -t 2000 && ffmpeg -framerate 60 -f x11grab -s 1920x1080 -i :0.0 -vf setpts=N/FR/TB $filename
+    # TODO: Enable HW acceleration in ffmpeg for this (and bump to 60fps?)
+    $(get_opts 1)) notify-send "Recording screen 1" "30fps / 3840x2160" -t 2000 && ffmpeg -framerate 30 -f x11grab -s 3840x2160 -i :0.0 -vf setpts=N/FR/TB -preset ultrafast $filename
     ;;
-    $(get_opts 2)) notify-send "Recording screen 2" "60fps / 1920x1080" -t 2000 && ffmpeg -framerate 60 -f x11grab -s 1920x1080 -i :0.0+1920,0 -vf setpts=N/FR/TB $filename
+$(get_opts 2)) pkill -STOP ffmpeg
     ;;
-$(get_opts 3)) pkill -STOP ffmpeg
+$(get_opts 3)) pkill -CONT ffmpeg
     ;;
-$(get_opts 4)) pkill -CONT ffmpeg
-    ;;
-$(get_opts 5)) pkill ffmpeg && notify-send "Recording saved!" "Saved at $filename"
+$(get_opts 4)) pkill ffmpeg && notify-send "Recording saved!" "Saved at $filename"
     ;;
     *) exit 1
     ;;
