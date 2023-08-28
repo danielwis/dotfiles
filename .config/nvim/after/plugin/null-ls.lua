e121caa44ac82b1440b17919cabde26c473ebf41
Daniel Williams <daniel_williams@live.com>
2023-08-28 10:46:57 +0200

    Fix shfmt options + add jsonlint

--- .config/nvim/after/plugin/null-ls.lua
+++ .config/nvim/after/plugin/null-ls.lua
@@ -6,10 +6,13 @@ local sources = {
   nls.builtins.formatting.black,
   nls.builtins.formatting.isort,
   -- shell
-  nls.builtins.formatting.shfmt,
+  nls.builtins.formatting.shfmt.with({
+    extra_args = { "-i", "2", "-bn", "-ci" },
+  }),
   nls.builtins.diagnostics.shellcheck,
-  -- markdown, yaml etc.
+  -- markdown, yaml, json etc.
   nls.builtins.formatting.prettierd,
+  nls.builtins.diagnostics.jsonlint,
 }
 
 nls.setup({ sources = sources })
