d8f3ac9b60e18a9658aecdd30edba02cf899eb85
Daniel Williams <daniel_williams@live.com>
2023-08-28 10:49:33 +0200

    Format plugins file

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -5,16 +5,16 @@
 local Plug = vim.fn['plug#']
 
 vim.call('plug#begin', '~/.config/nvim/plugged')
-Plug 'lervag/vimtex' -- Lots of LaTeX support
-Plug 'tpope/vim-surround' -- Surround text objects with things (parens, quotes etc.)
-Plug 'tpope/vim-repeat' -- Extend the . command to things like vim-surround
-Plug 'tpope/vim-sleuth' -- Automatically set tab width/spaces
-Plug 'romainl/vim-cool' -- No more lingering hl after search
+Plug 'lervag/vimtex'                    -- Lots of LaTeX support
+Plug 'tpope/vim-surround'               -- Surround text objects with things (parens, quotes etc.)
+Plug 'tpope/vim-repeat'                 -- Extend the . command to things like vim-surround
+Plug 'tpope/vim-sleuth'                 -- Automatically set tab width/spaces
+Plug 'romainl/vim-cool'                 -- No more lingering hl after search
 Plug 'inkarkat/vim-ReplaceWithRegister' -- Replace motion with register without intermediate deletion
-Plug 'echasnovski/mini.ai' -- Extend `i` and `a` text objects
+Plug 'echasnovski/mini.ai'              -- Extend `i` and `a` text objects
 
-Plug 'zegervdv/nrpattern.nvim' -- Extend <C-x> and <C-a> functionality to custom patterns etc
-Plug 'rcarriga/nvim-notify' -- Notifications
+Plug 'zegervdv/nrpattern.nvim'          -- Extend <C-x> and <C-a> functionality to custom patterns etc
+Plug 'rcarriga/nvim-notify'             -- Notifications
 
 Plug 'nvim-lualine/lualine.nvim'
 Plug 'nvim-tree/nvim-web-devicons' -- Icons
@@ -33,7 +33,7 @@ Plug 'numToStr/Comment.nvim'
 
 -- Tab-/bufferline
 Plug('nvim-tree/nvim-web-devicons') -- For coloured icons
-Plug('akinsho/bufferline.nvim', { [ 'tag' ] = 'v3.*' })
+Plug('akinsho/bufferline.nvim', { ['tag'] = 'v3.*' })
 
 -- Indent prettifyer
 Plug 'lukas-reineke/indent-blankline.nvim'
@@ -53,11 +53,11 @@ Plug 'jose-elias-alvarez/null-ls.nvim'
 
 -- Autocompletion and its sources
 Plug 'hrsh7th/nvim-cmp'
-Plug 'hrsh7th/cmp-buffer'
-Plug 'hrsh7th/cmp-path'
 Plug 'hrsh7th/cmp-nvim-lsp'
 Plug 'hrsh7th/cmp-nvim-lua'
 Plug 'hrsh7th/cmp-nvim-lsp-signature-help'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/cmp-path'
 Plug 'f3fora/cmp-spell'
 
 Plug('iamcco/markdown-preview.nvim', { ['do'] = 'cd app && yarn install' })
