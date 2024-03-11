1cd994eb66c5d9881e571ebcba8e14a8717d2988
Daniel Williams <daniel_williams@live.com>
2024-03-11 23:05:56 +0100

    Well hello there Wayland

--- dot_config/i3blocks/scripts/executable_pmenu.sh
+++ dot_config/i3blocks/scripts/executable_pmenu.sh
@@ -9,19 +9,15 @@ echo "#dddddd"
 if [ "$button" = "1" ]; then
     uptime=$(uptime -p | sed 's/up //g')
     usr="Daniel"
-    shutdown=""
-    reboot=""
-    suspend=""
-    lock=""
-    logout="󰍃"
+    shutdown=" Shutdown"
+    reboot=" Reboot"
+    suspend=" Suspend"
+    lock=" Lock"
+    logout="󰍃 Log out"
 
     options=$(echo "$lock\n$suspend\n$logout\n$reboot\n$shutdown")
 
-    action=$(echo -e $options | rofi \
-        -dmenu \
-        -p "Goodbye, $usr" \
-        -mesg "Uptime: $uptime" \
-        -theme powermenu.rasi)
+    action=$(echo -e $options | tofi --prompt-text "..." --height "36%" --font-size 36)
 
     case $action in
         $shutdown)
@@ -31,11 +27,11 @@ if [ "$button" = "1" ]; then
             systemctl reboot
             ;;
         $suspend)
-            ~/.config/i3blocks/scripts/lock.sh
+            swaylock -f
             systemctl suspend
             ;;
         $lock)
-            ~/.config/i3blocks/scripts/lock.sh
+            swaylock -f
             ;;
         $logout)
             i3-msg exit
