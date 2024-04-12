bbb0e401526da836fb268e21423d2548883788e3
Daniel Williams <daniel_williams@live.com>
2024-04-12 13:02:06 +0200

    Use wayland-friendly zathura viewer for vimtex

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -77,7 +77,7 @@ vim.cmd[[hi Folded ctermbg=Black]]
 --- Plugin options ---
 ----------------------
 vim.g.tex_flavor='latex'
-vim.g.vimtex_view_method='zathura'
+vim.g.vimtex_view_method='zathura_simple'
 vim.g.vimtex_quickfix_mode=0
 vim.g.UltiSnipsJumpForwardTrigger = "<tab>"
 vim.g.UltiSnipsJumpBackwardTrigger = "<s-tab>"
