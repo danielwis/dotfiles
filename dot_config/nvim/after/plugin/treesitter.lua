76543c9fcfa9199f32fcd09926a5a709e092a7b1
Daniel Williams <daniel_williams@live.com>
2024-05-22 01:29:54 +0200

    Delegate latex syntax highlighting to vimtex

--- dot_config/nvim/after/plugin/treesitter.lua
+++ dot_config/nvim/after/plugin/treesitter.lua
@@ -16,6 +16,7 @@ require 'nvim-treesitter.configs'.setup {
   highlight = {
     -- `false` will disable the whole extension
     enable = true,
+    disable = { 'latex' },
 
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
