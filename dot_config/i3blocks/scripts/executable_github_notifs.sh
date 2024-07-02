27ad46cee1ca39751b75ff9290058b6d1d3035d2
Daniel Williams <daniel_williams@live.com>
2024-07-03 00:16:53 +0200

    [bar/github-notifs] Don't error on curl failures

--- dot_config/i3blocks/scripts/executable_github_notifs.sh
+++ dot_config/i3blocks/scripts/executable_github_notifs.sh
@@ -6,21 +6,33 @@ cwd=$(dirname "$0")
 url="https://github.com/notifications"
 
 notifs=$(curl -L --silent \
-	-H "Accept: application/vnd.github+json" \
-	-H "Authorization: Bearer $(cat ~/Documents/personal/gh.token)" \
-	-H "X-GitHub-Api-Version: 2022-11-28" \
-	"https://api.github.com/notifications?all=false" | jq 'length')
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $(cat ~/Documents/personal/gh.token)" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  "https://api.github.com/notifications?all=false" | jq 'length')
 
-echo "$NOTIFICATION_ICON $notifs"
-echo "$NOTIFICATION_ICON $notifs"
+if [ -n "$notifs" ]; then
+  echo "$NOTIFICATION_ICON $notifs"
+  echo "$NOTIFICATION_ICON $notifs"
 
-if [ "$button" = "1" ]; then
-	notify-send "$notifs new notification(s)" "<a href=\"$url\">Click to open in browser</a>"
-elif [ "$button" = "2" ]; then
-	# Just open
-	xdg-open $url
+  # Colour
+  if [ "$notifs" = "0" ]; then
+    echo "$PASTEL_GREEN"
+  elif [ "$notifs" -lt "5" ]; then
+    echo "$PASTEL_YELLOW"
+  else
+    echo "$PASTEL_RED"
+  fi
+else
+  echo "$NOTIFICATION_ICON (?)"
+  echo "$NOTIFICATION_ICON (?)"
+  echo "#ffac1c" # orange
+  # echo "#ffbf00" # orange
 fi
 
-[ "$notifs" = "0" ] && echo "$PASTEL_GREEN" && exit
-[ "$notifs" -lt "5" ] && echo "$PASTEL_YELLOW" && exit
-echo "$PASTEL_RED" && exit
+if [ "$button" = "1" ]; then
+  notify-send "$notifs new notification(s)" "<a href=\"$url\">Click to open in browser</a>"
+elif [ "$button" = "2" ]; then
+  # Just open
+  xdg-open $url
+fi
