46c6f728badfd327e3eba095151505ec423c1486
Daniel Williams <daniel_williams@live.com>
2024-07-12 16:28:41 +0200

    [nvim] Remove isort from null-ls (ruff handles this)

--- dot_config/nvim/after/plugin/null-ls.lua
+++ dot_config/nvim/after/plugin/null-ls.lua
@@ -2,8 +2,6 @@ local nls = require("null-ls")
 
 
 local sources = {
-  -- python
-  nls.builtins.formatting.isort,
   -- shell
   nls.builtins.formatting.shfmt.with({
     extra_args = { "-i", "2", "-bn", "-ci" },
